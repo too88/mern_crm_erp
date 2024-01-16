aa59e668b2f2263f38ee89617fa03b13635c4d31
toohuynh <tuhuynh1902@gmail.com>
2024-01-16 23:39:45 +0700

    config homepage path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,10 +11,6 @@ export default ({ mode }) => {
       : "http://localhost:8888";
 
   const config = {
-    build: {
-      outDir: 'dist',
-      assetsDir: './assets',
-    },
     plugins: [react()],
     resolve: {
       base: "/",
