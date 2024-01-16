e2c089e6d8b8c4a087da1946afac8e7aa72f9bfc
too88 <too88@users.noreply.github.com>
2024-01-16 13:51:49 +0000

    deploy: d574814b17fe08719aed0e4a0937f94d3fb348d4

--- assets/index-9jJMQ8No.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-CXyQeB9U.js";function r(){return n.jsx("div",{children:"Company"})}export{r as default};
--- assets/index-XUQ7yzeC.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CXyQeB9U.js";function o(){return r.jsx("div",{children:"Product"})}export{o as default};
