ce35edaae8fb1a0da579456e94b7f78c6b6613e8
toohuynh <tuhuynh1902@gmail.com>
2024-01-19 18:40:03 +0700

    build company component and its field configurations

---
+++ frontend/src/pages/Company/config.js
@@ -0,0 +1,18 @@
+export const fields = {
+    name: {
+        type: 'string',
+        require: true,
+    },
+    country: {
+        type: 'country'
+    },
+    phone: {
+        type: 'phone'
+    },
+    email: {
+        type: 'email'
+    },
+    website: {
+        type: 'url'
+    },
+}
--- frontend/src/pages/Company/index.jsx
+++ frontend/src/pages/Company/index.jsx
@@ -1,5 +1,27 @@
 import CrudModule from "@/modules/CrudModule";
+import { fields } from "./config";
+
+const COMPANY = "company";
 
 export default function Company() {
-  return <CrudModule />;
+  const entity = COMPANY;
+
+  const labels = {
+    PANEL_TITLE: "Company",
+    TABLE_NAME: "Company List",
+    ADD_NEW_ENTITY: "Add New Entity",
+    ENTITY_NAME: "Company",
+  };
+
+  const configPage = {
+    ...labels,
+    entity,
+  };
+
+  const config = {
+    ...configPage,
+    fields,
+  };
+
+  return <CrudModule config={config} />;
 }
