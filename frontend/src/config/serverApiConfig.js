4969e8b58f6fe9c1705da8e0e5e267b16c5f9fb7
toohuynh <tuhuynh1902@gmail.com>
2024-01-22 14:50:29 +0700

    config remote proxy for fe

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE === "remote"
-    ? import.meta.env.VITE_BACKEND_SERVER + "/api"
+    ? import.meta.env.VITE_BACKEND_SERVER_REMOTE + "/api"
     : "http://localhost:8888/api/";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default ({ mode }) => {
 
   const proxy_url =
     process.env.VITE_DEV_REMOTE === "remote"
-      ? process.env.VITE_BACKEND_SERVER
+      ? process.env.VITE_BACKEND_SERVER_REMOTE
       : "http://localhost:8888";
 
   const config = {
